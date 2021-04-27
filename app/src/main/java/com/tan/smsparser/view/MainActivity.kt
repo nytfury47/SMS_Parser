f4baeb879ca42ca7f623dcc78cb3b5383f2d2f50
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2021-04-27 16:05:05 +0900

    setup MVVM (https://dev.to/whatminjacodes/simple-example-of-mvvm-architecture-in-kotlin-4j5b)

---
+++ app/src/main/java/com/tan/smsparser/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tan.smsparser.view
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+import com.tan.smsparser.R
+
+/*
+ *      MainActivity
+ *      - opens our fragment which has the UI
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            // Adds our fragment
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MainFragment>(R.id.fragment_container_view)
+            }
+        }
+    }
+}

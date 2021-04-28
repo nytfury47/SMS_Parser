40bb0c2e26daf3ed399e2ae0aae60322eb6ba9f8
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2021-04-28 21:48:17 +0900

    setup permissions check

--- app/src/main/java/com/tan/smsparser/view/MainFragment.kt
+++ app/src/main/java/com/tan/smsparser/view/MainFragment.kt
@@ -1,12 +1,20 @@
 package com.tan.smsparser.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+
+import com.afollestad.assent.GrantResult
+import com.afollestad.assent.Permission
+import com.afollestad.assent.askForPermissions
+import com.afollestad.materialdialogs.MaterialDialog
 
 import com.tan.smsparser.databinding.FragmentMainBinding
 import com.tan.smsparser.viewmodel.MainViewModel
@@ -18,12 +26,18 @@ import com.tan.smsparser.viewmodel.MainViewModel
  */
 class MainFragment: Fragment() {
 
+    //region Private properties
+
     // View Binding
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
     // Create a viewModel
     private val viewModel: MainViewModel by activityViewModels()
+
+    //endregion
+
+    //region View Life Cycle
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,9 +60,13 @@ class MainFragment: Fragment() {
         _binding = null
     }
 
+    //endregion
+
+    //region Private functions
+
     // Setup the button in our fragment to call getUpdatedText method in viewModel
     private fun setupClickListeners() {
-        binding.fragmentButton.setOnClickListener { viewModel.getUpdatedText() }
+        binding.fragmentButton.setOnClickListener { readSMS() }
     }
 
     // Observer is waiting for viewModel to update our UI
@@ -56,5 +74,50 @@ class MainFragment: Fragment() {
         viewModel.uiTextLiveData.observe(viewLifecycleOwner, { updatedText ->
             binding.fragmentTextView.text = updatedText
         })
+    }
+
+    private fun readSMS() {
+        // Perform viewModel action only when READ_SMS permission is granted
+        // If READ_SMS permission is not granted, ask user to grant permission from Settings page
+        askForPermissions(PERMISSION_READ_SMS) { result ->
+            if (result[PERMISSION_READ_SMS] == GrantResult.GRANTED) {
+                viewModel.getUpdatedText()
+            } else {
+                requestForPermissions()
+            }
+        }
+    }
+
+    private fun requestForPermissions() {
+        activity?.let {
+            MaterialDialog(it).show {
+                title(text = DIALOG_TITLE_TEXT)
+                message(text = DIALOG_MESSAGE_TEXT)
+                positiveButton(text = DIALOG_POSITIVE_BUTTON_TEXT) {
+                    openAppSystemSettings()
+                }
+                negativeButton(text = DIALOG_NEGATIVE_BUTTON_TEXT) {
+                    // Do nothing
+                }
+            }
+        }
+    }
+
+    private fun openAppSystemSettings() {
+        activity?.let {
+            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", it.packageName, null)
+            })
+        }
+    }
+
+    //endregion
+
+    companion object {
+        private val PERMISSION_READ_SMS = Permission.READ_SMS
+        private const val DIALOG_TITLE_TEXT = "Permission to read SMS"
+        private const val DIALOG_MESSAGE_TEXT = "Please grant this app the permission to read SMS from the Settings page."
+        private const val DIALOG_POSITIVE_BUTTON_TEXT = "OK"
+        private const val DIALOG_NEGATIVE_BUTTON_TEXT = "Cancel"
     }
 }
